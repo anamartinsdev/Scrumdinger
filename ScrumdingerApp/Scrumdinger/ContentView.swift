ccb8de224646ed6715b9618ecd2a0614364895af
Ana Carolina Martins Pessoa <anamartinspessoa@icloud.com>
2022-11-06 23:02:38 -0300

    Create base project

---
+++ ScrumdingerApp/Scrumdinger/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Ana Carolina Martins Pessoa on 06/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .center) {
+                    Label("Seconds Elapsed", systemImage: "circle" ).labelStyle(TitleOnlyLabelStyle())
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                        .padding(.trailing)
+                        .font(.caption)
+                }
+                Spacer()
+                VStack {
+                    Label("Seconds Remaining", systemImage: "circle")
+                        .labelStyle(TitleOnlyLabelStyle())
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                        .font(.caption)
+                        .padding(.leading)
+                }
+            }
+            Circle()
+            HStack {
+                Label("Speaker 1 of 3", systemImage: "circle").labelStyle(TitleOnlyLabelStyle())
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrumdingerApp/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ana Carolina Martins Pessoa on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
